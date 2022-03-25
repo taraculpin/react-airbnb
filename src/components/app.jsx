23441f7b388cf6f5728bcd30b1653bcec5ccdf3f
Tara Culpin <taraculpin@gmail.com>
2022-03-25 22:51:30 +0000

    Added FlatList to App render call

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,13 +2,21 @@
 import React, { Component } from "react";
 import FlatList from "./flat_list";
 import MapContainer from "./map_container";
+import flats from "../../data/flats";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        flats: flats
+      }
+  }
+
   render() {
     return (
       <div>
         <div className = "flat-list">
-          <FlatList />
+          <FlatList flats={this.state.flats}/>
         </div>
         <div className = "map-container">
           <MapContainer />
