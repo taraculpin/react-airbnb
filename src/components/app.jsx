19434f5ea9bf54a7c34bdc68aa441ee36a98cf73
Tara Culpin <taraculpin@gmail.com>
2022-03-29 11:49:57 +0100

    added markers to the map

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,11 +4,14 @@ import FlatList from "./flat_list";
 import flats from "../../data/flats";
 import MapContainer from "./map_container";
 
+const markers = flats.map(flat => [flat.lat, flat.lng])
+
 class App extends Component {
   constructor(props) {
     super(props);
       this.state = {
         flats: flats,
+        markers: markers
       }
   }
 
@@ -16,7 +19,7 @@ class App extends Component {
     return (
       <div>
         <FlatList flats={this.state.flats}/>
-        <MapContainer />
+        <MapContainer markers={this.state.markers} />
       </div>
     )
   }
--- src/components/map_container.jsx
+++ src/components/map_container.jsx
@@ -1,9 +1,7 @@
 
 import React, { Component } from "react";
 import mapboxgl from '!mapbox-gl';
-import ReactMapGL, { Marker } from 'react-map-gl';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoidGFyYW1hY3UiLCJhIjoiY2t6OGp6Z3QyMGEwcTJ1bzF6bnZjZmtjdyJ9.PGyURIG71drebfI-t81J4Q';
 
 class MapContainer extends Component {
   constructor(props) {
@@ -16,7 +14,25 @@ class MapContainer extends Component {
     this.mapContainer = React.createRef();
   }
 
+  #addMarkersToMap(map) {
+    this.props.markers.forEach(marker => {
+    // const popup = new mapboxgl.Popup().setHTML(marker.info_window)
+
+    const customMarker = document.createElement("div")
+    customMarker.className = "marker"
+    customMarker.style.backgroundImage = `url('https://www.svgrepo.com/show/22031/home-icon-silhouette.svg')`
+    customMarker.style.backgroundSize = "contain"
+    customMarker.style.width = "25px"
+    customMarker.style.height = "25px"
+
+    new mapboxgl.Marker(customMarker)
+      .setLngLat([ marker[1], marker[0] ])
+      .addTo(map)
+    });
+  }
+
   componentDidMount() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoidGFyYW1hY3UiLCJhIjoiY2t6OGp6Z3QyMGEwcTJ1bzF6bnZjZmtjdyJ9.PGyURIG71drebfI-t81J4Q';
     const { lng, lat, zoom } = this.state;
     const map = new mapboxgl.Map({
       container: this.mapContainer.current,
@@ -24,7 +40,7 @@ class MapContainer extends Component {
       center: [lng, lat],
       zoom: zoom
     });
-
+    this.#addMarkersToMap(map);
 
     map.on('move', () => {
       this.setState({
